773fdbe41f0889aa3f7824de724cb72f550a609d
Grenky <demosoft62@gmail.com>
2023-12-21 18:47:24 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './components/Home';
+import './components/styles/Normalize.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return(
+        <header>
+            <div className="logo">
+                Anna Kakama
+            </div>
+        </header>
+    )
+}
