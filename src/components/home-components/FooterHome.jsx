9945e965748576b04e131ea0e61fcedc5ab3ad88
Grenky <demosoft62@gmail.com>
2024-01-10 15:59:48 +0200

    12

--- src/components/home-components/FooterHome.jsx
+++ src/components/home-components/FooterHome.jsx
@@ -4,7 +4,7 @@ import './FooterHome.css';
 export default function FooterHome() {
     return(
         <div className="footerhome-wrapper">
-            <h2 className='footerhome-title'>Contactez Victor Bellot, votre photographe culinaire à Paris</h2>
+            <h2 className='footerhome-title'>Contact Anna Kakama, votre photographe culinaire à Paris</h2>
             <p>Photographe - contact@victorbellot.com - 32 Rue du dragon 75006 Paris</p>
             <img src='./images/inst.png' className='img9' alt='img'></img>
             <p>2017 &nbsp; créé par Victor Bellot tous droits réservés.</p>
--- src/components/home-components/SecondHome.jsx
+++ src/components/home-components/SecondHome.jsx
@@ -3,21 +3,13 @@ import './SecondHome.css';
 
 export default function SecondHome() {
     return(
-        <div className="secondhome-wrapper">
-            <h1 className='secondhome-maintitle'>Passion, création, précision, les trois piliers de ce métier.</h1>
-             
-            <div className='secondhome-text-box'>   
-                <div className='secondhome-text'>
-                <h2 className='secondhome-title'>LE SAVOIR-FAIRE D'UN PHOTOGRAPHE CULINAIRE</h2>  
-                    Si vous souhaitez vous offrir les services d'un photographe culinaire à Paris vous êtes ici au bon endroit. Tout d'abord il convient d'en dire plus sur la photographie culinaire afin que vous puissiez comprendre les spécificités de ce métier, bien différent de celui d'un photographe "généraliste".
-                    Le photographe culinaire n'est jamais seul, son travail n'a pas lieu d'être sans la créativité et le talent des chefs cuisiniers qui composent les plats.
-                    Il s'agit donc d'une prestation à réaliser en harmonie totale avec le créateur du plat, parfois le directeur artistique ou le styliste culinaire, dans le cas d'ouvrages de cuisine par exemple.
-                    L'expertise du photographe culinaire consiste à magnifier ces plats, à mettre en valeur et en lumière la gastronomie.
-                    La connaissance technique doit être parfaite : éclairages naturels ou artificiels, focales utilisées, post-traitement.
-                    L'erreur n'a pas sa place.
-                </div>
-                <img src='./images/img8.JPG' className='img8' alt='img'></img>
-            </div>   
+        <div className="secondhome-wrapper">   
+                <img src='./images/img11.JPG' className='img11' alt='img'></img>
+                <img src='./images/img12.JPG' className='img12' alt='img'></img>
+                <img src='./images/img13.JPG' className='img13' alt='img'></img>
+                <img src='./images/img14.JPG' className='img14' alt='img'></img>
+                <img src='./images/img15.JPG' className='img15' alt='img'></img>
+                <img src='./images/img16.JPG' className='img16' alt='img'></img>
         </div>
     )
 }
