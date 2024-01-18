8213a7fdd77747d58f84b87cb2b19a05d9a57eb8
Grenky <demosoft62@gmail.com>
2024-01-18 16:50:26 +0200

    16

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,17 +1,30 @@
 import '../styles/Portfolio.css';
 import { Link } from 'react-router-dom';
-
+import Carousel from './carousel/Carousel';
 
 export default function Portfolio() {
+
+
+
     return(
         <div className="portfolio-wrapper">
             <div className="portfolio">
-                <Link className='people-link' alt='people' to='/People'>People</Link>
-                <Link className='animals-link' alt='animals' to='/Animals'>Animals</Link> 
-                <Link className='nature-link' alt='nature' to='/Nature'>Nature</Link>             
+                <Link className='people-link' alt='people' to='/People'>
+                    <button>People</button>
+                </Link>
+                <Link className='animals-link' alt='animals' to='/Animals'>
+                    <button>Animals</button>
+                </Link> 
+                <Link className='nature-link' alt='nature' to='/Nature'>
+                    <button>Nature</button>
+                </Link>             
             </div>
             <div className='section-wrapper'>
-           
+                <Carousel className='slider'>
+                    <div className='item item-1'><img className='slider-img1' src='./images/slider-img1.JPG' alt='img'></img></div>
+                    <div className='item item-2'><img className='slider-img2' src='./images/slider-img2.JPG' alt='img'></img></div>
+                    <div className='item item-3'><img className='slider-img3' src='./images/slider-img3.JPG' alt='img'></img></div>
+                </Carousel>
             </div>
         </div>
     )
