10275ad37449d1070360af2d39a67bd96397654c
Grenky <demosoft62@gmail.com>
2024-02-14 16:27:06 +0200

    22

--- src/components/home-components/FirstHome.jsx
+++ src/components/home-components/FirstHome.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
-import './FirstHome.css';
+import './media.css';
 import Modal from './Modal';
+import './FirstHome.css';
+
 
 export default function FirstHome() {
 
@@ -17,17 +19,17 @@ export default function FirstHome() {
         <div className="firsthome-wrapper">
             <div className='firsthome-title-box1'>  
                 <img onClick={() => handleClick('./images/img1.JPG')} src='./images/img1.JPG' className='img1' alt='img'></img>
-                <img onClick={() => handleClick('./images/img3.JPG')} src='./images/img3.JPG' className='img3' alt='img'></img>
-                <img onClick={() => handleClick('./images/img2.JPG')} src='./images/img2.JPG' className='img2' alt='img'></img>
-                <img onClick={() => handleClick('./images/IMG_3064.JPG')} src='./images/IMG_3064.JPG' className='img2' alt='img'></img>
+                <img onClick={() => handleClick('./images/img3.JPG')} src='./images/img3.JPG' className='img2' alt='img'></img>
+                <img onClick={() => handleClick('./images/img2.JPG')} src='./images/img2.JPG' className='img3' alt='img'></img>
+                <img onClick={() => handleClick('./images/IMG_3064.JPG')} src='./images/IMG_3064.JPG' className='img4' alt='img'></img>
               
             </div>
             
             <div className='firsthome-title-box2'>  
-                <img onClick={() => handleClick('./images/img4.JPG')} src='./images/img4.JPG' className='img4' alt='img'></img>
+                <img onClick={() => handleClick('./images/DSC_7131 full.jpg')} src='./images/DSC_7131 full.jpg' className='img5' alt='img'></img>
                 <img onClick={() => handleClick('./images/img6.JPG')} src='./images/img6.JPG' className='img6' alt='img'></img>
-                <img onClick={() => handleClick('./images/img10.JPG')} src='./images/img10.JPG' className='img10' alt='img'></img>
-                <img onClick={() => handleClick('./images/DSC_7225.JPG')} src='./images/DSC_7225.JPG' className='img2' alt='img'></img>
+                <img onClick={() => handleClick('./images/img10.JPG')} src='./images/img10.JPG' className='img7' alt='img'></img>
+                <img onClick={() => handleClick('./images/DSC_7225.JPG')} src='./images/DSC_7225.JPG' className='img8' alt='img'></img>
             </div>
         <Modal active={modalActive} setActive={setModalActive}>
             <img  src={imgSrc}  alt='img' className='pop-img'></img>
--- src/components/home-components/InfoHome.jsx
+++ src/components/home-components/InfoHome.jsx
@@ -18,7 +18,7 @@ export default function InfoHome() {
                 <img onClick={() => handleClick('./images/img17.JPG')} src='./images/img17.JPG' className='img17' alt='img'></img>
                 <img onClick={() => handleClick('./images/img18.JPG')} src='./images/img18.JPG' className='img18' alt='img'></img>
                 <img onClick={() => handleClick('./images/DSC_7212.JPG')} src='./images/DSC_7212.JPG' className='img19' alt='img'></img>
-                <img onClick={() => handleClick('./images/img19.JPG')} src='./images/img19.JPG' className='img19' alt='img'></img>
+                <img onClick={() => handleClick('./images/img19.JPG')} src='./images/img19.JPG' className='img20' alt='img'></img>
             </div>
         <Modal active={modalActive} setActive={setModalActive}>
             <img  src={imgSrc}  alt='img' className='pop-img'></img> 
